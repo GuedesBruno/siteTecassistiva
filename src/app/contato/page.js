960516db793aa37e45775ac7ad4d06205d4926f0
GuedesBruno <brunoguedes@tecassistiva.com.br>
2025-10-17 16:21:56 -0300

    add - map

--- src/app/contato/page.js
+++ src/app/contato/page.js
@@ -36,6 +36,21 @@ export default function ContatoPage() {
           <p className="text-gray-700">Seg-Qui 8h-18h e Sexta 8h-17h</p>
         </div>
       </section>
+
+      <section className="mt-12">
+        <h2 className="text-3xl font-extrabold text-center mb-8">Nossa Localização</h2>
+        <div className="w-full overflow-hidden rounded-lg shadow-lg">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.9935919417994!2d-46.642828889231275!3d-23.604562763091913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a37343224f3%3A0xe4270a2659bfe78f!2sTecassistiva%20-%20Tecnologia%20%26%20Acessibilidade!5e0!3m2!1spt-BR!2sbr!4v1760723939741!5m2!1spt-BR!2sbr" 
+            width="100%" 
+            height="450" 
+            className="border-0" 
+            allowFullScreen="" 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
+      </section>
     </div>
   );
 }
