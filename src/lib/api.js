0a7d4d8db1610d53b6a04d8f25b978abfcb150fb
GuedesBruno <brunoguedes@tecassistiva.com.br>
2025-10-30 11:45:16 -0300

    include - ordem atas

--- src/lib/api.js
+++ src/lib/api.js
@@ -289,7 +289,8 @@ export async function getAllSimplePages() {
 }
 
 export async function getProductsByManufacturerSlug(slug) {
-    const response = await fetchAPI(`/api/produtos?filters[fabricante][slug][$eq]=${slug}&populate=*`);
+    // Usando o campo de relação correto do Strapi
+    const response = await fetchAPI(`/api/produtos?filters[relacao_fabricante][slug][$eq]=${slug}&populate=*`);
     return normalizeDataArray(response);
 }
 
