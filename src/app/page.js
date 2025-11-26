8ab7108ca1573641313653e5e3472c611390742e
GuedesBruno <brunoguedes@tecassistiva.com.br>
2025-11-26 08:29:28 -0300

    fix - home

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default async function Home() {
       <section className="bg-tec-blue-light text-white py-16">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <h2 className="text-4xl font-extrabold mb-4">Tecassistiva</h2>
-          <p className="mx-auto leading-relaxed text-xl">
+          <p className="mx-auto text-justify leading-relaxed text-xl">
             A Tecassistiva é uma empresa brasileira especializada em soluções de tecnologia assistiva voltadas à inclusão educacional e social de pessoas com deficiência. Atuamos há mais de duas décadas oferecendo equipamentos, softwares e serviços que ampliam a autonomia, o acesso à informação e as oportunidades de aprendizagem para estudantes, profissionais e instituições em todo o país.
             Nosso compromisso é entregar tecnologia que transforma vidas. Por isso desenvolvemos e representamos soluções de alto desempenho para pessoas com deficiência visual e outras necessidades específicas, atendendo escolas, secretarias de educação, universidades, bibliotecas, empresas e órgãos públicos em geral com excelência e responsabilidade.
           </p>
